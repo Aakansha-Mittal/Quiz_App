1378a8f104ad189050cfd2ffbca9f82afb447fca
Aakansha Mittal <aakanshamittal8880@gmail.com>
2023-08-14 12:59:56 +0530

    Initial Commit

--- app/src/main/java/com/example/quiz_alertdialogbox/MainActivity.kt
+++ app/src/main/java/com/example/quiz_alertdialogbox/MainActivity.kt
@@ -13,8 +13,8 @@ class MainActivity : AppCompatActivity() {
     var ans1 = ""
     var ans2 = ""
     var ans3 = ""
-
     var count = 0
+    var attempted = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +27,13 @@ class MainActivity : AppCompatActivity() {
             builder1.setTitle("121 Divided by 11 is ")
             builder1.setSingleChoiceItems(options, 5, DialogInterface.OnClickListener { dialog, which ->
                 ans1 = options[which]
-                Toast.makeText(this, "You selected ${options[which]}", Toast.LENGTH_SHORT).show()
+
             })
             builder1.setPositiveButton("Submit", DialogInterface.OnClickListener { dialog, i ->
                 if(ans1=="11"){
                     count+=1
                 }
+                attempted++
                 Toast.makeText(this, "Submitted", Toast.LENGTH_SHORT).show()
             })
             builder1.setNegativeButton("Decline", DialogInterface.OnClickListener {dialog, i ->
@@ -47,12 +48,13 @@ class MainActivity : AppCompatActivity() {
             builder1.setTitle("60 Times of 8 Equals to ")
             builder1.setSingleChoiceItems(options, 5, DialogInterface.OnClickListener { dialog, which ->
                 ans2 = options[which]
-                Toast.makeText(this, "You selected ${options[which]}", Toast.LENGTH_SHORT).show()
+
             })
             builder1.setPositiveButton("Submit", DialogInterface.OnClickListener { dialog, i ->
                 if(ans2=="480"){
                     count+=1
                 }
+                attempted++
                 Toast.makeText(this, "Submitted", Toast.LENGTH_SHORT).show()
             })
             builder1.setNegativeButton("Decline", DialogInterface.OnClickListener {dialog, i ->
@@ -64,14 +66,19 @@ class MainActivity : AppCompatActivity() {
         binding.que3.setOnClickListener{
             val builder1 = AlertDialog.Builder(this)
             val options = arrayOf("1", "2", "9", "7", "21")
+            val selectedItems = mutableListOf<String>()
+            val answerList = arrayOf("2", "7")
+
             builder1.setTitle("Select the prime numbers from given numbers ")
             builder1.setMultiChoiceItems(options, null, DialogInterface.OnMultiChoiceClickListener { dialog, i, isChecked ->
-                ans3=options[i]
+                selectedItems.add(options[i])
             })
+
             builder1.setPositiveButton("Submit", DialogInterface.OnClickListener { dialog, i ->
-                if(ans3=="2" || ans3=="7"){
+                if((selectedItems.size==2) && ("2" in selectedItems) && ("7" in selectedItems)){
                     count+=1
                 }
+                attempted++
                 Toast.makeText(this, "Submitted", Toast.LENGTH_SHORT).show()
             })
             builder1.setNegativeButton("Decline", DialogInterface.OnClickListener {dialog, i ->
@@ -80,8 +87,51 @@ class MainActivity : AppCompatActivity() {
             builder1.show()
         }
 
-        binding.btnExit.setOnClickListener{
-            Toast.makeText(this, "Your score is $count out of 3", Toast.LENGTH_LONG).show()
+        binding.que4.setOnClickListener {
+            val builder1 = AlertDialog.Builder(this)
+            val options = arrayOf("225", "124", "97", "121")
+            builder1.setTitle("Multiple of 3 is equals to ")
+            builder1.setSingleChoiceItems(options, 5, DialogInterface.OnClickListener { dialog, which ->
+                ans2=options[which]
+            })
+            builder1.setPositiveButton("Submit", DialogInterface.OnClickListener { dialog, which ->
+                if(ans2=="225")
+                    count++
+                attempted++
+                Toast.makeText(this, "Submitted", Toast.LENGTH_SHORT).show()
+            })
+            builder1.setNegativeButton("Decline", DialogInterface.OnClickListener { dialog, which ->
+
+            })
+            builder1.show()
+        }
+
+        binding.que5.setOnClickListener {
+            val builder1 = AlertDialog.Builder(this)
+            val options = arrayOf("228", "156", "146", "166")
+            builder1.setTitle("12 times 13 equals to ")
+            builder1.setSingleChoiceItems(options, 5, DialogInterface.OnClickListener { dialog, which ->
+                ans2=options[which]
+            })
+            builder1.setPositiveButton("Submit", DialogInterface.OnClickListener { dialog, which ->
+                if(ans2=="156")
+                    count++
+                attempted++
+                Toast.makeText(this, "Submitted", Toast.LENGTH_SHORT).show()
+            })
+            builder1.setNegativeButton("Decline", DialogInterface.OnClickListener { dialog, which ->
+
+            })
+            builder1.show()
+        }
+
+
+        binding.btnMarks.setOnClickListener{
+            Toast.makeText(this, "Your score is $count out of $attempted", Toast.LENGTH_LONG).show()
+        }
+
+        binding.btnExit.setOnClickListener {
+            finish()
         }
 
     }
